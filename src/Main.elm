27e678dea10abd1f1a40df302f9fc2756dbee01d
Bill Peregoy <bill@gnar.dog>
2018-06-05 22:37:25 -0400

    Initial commit

--- src/Main.elm
+++ src/Main.elm
@@ -1,39 +1,98 @@
 module Main exposing (..)
 
 import Html exposing (..)
-import Html.Attributes exposing (..)
-import Utils
+import Array
+import Time
 
 
 main : Program Never Model Msg
 main =
     Html.program
-        { init = init
+        { init = init ! []
         , view = view
         , update = update
         , subscriptions = subscriptions
         }
 
 
-
--- Model
-
-
 type alias Model =
-    { name : String }
+    { pc : Int
+    , ram : Array.Array Instruction
+    , clkCycle : Int
+    , tCycle : TCycle
+    , mCycle : MCycle
+    , registers : RegisterValues
+    }
 
 
-init : ( Model, Cmd Msg )
+type Register
+    = A
+    | B
+    | C
+    | D
+    | E
+    | H
+    | L
+
+
+type TCycle
+    = T1
+    | T2
+    | T3
+    | T4
+
+
+type MCycle
+    = M1
+    | M2
+    | M3
+
+
+type Instruction
+    = NOP
+    | HALT
+    | LD Register Register
+    | LDI Register Int
+    | ADD Register
+    | JRZ Int
+
+
+type alias RegisterValues =
+    { a : Int
+    , b : Int
+    , c : Int
+    , d : Int
+    , e : Int
+    , h : Int
+    , l : Int
+    }
+
+
+init : Model
 init =
-    Model "world" ! []
+    { pc = 0
+    , ram = initRam
+    , clkCycle = 0
+    , tCycle = T1
+    , mCycle = M1
+    , registers = RegisterValues 0 0 0 0 0 0 0
+    }
 
 
-
--- Update
+initRam : Array.Array Instruction
+initRam =
+    Array.fromList
+        [ LDI A 10
+        , LDI B -1
+        , ADD B
+        , JRZ -1
+        , HALT
+        ]
 
 
 type Msg
     = NoOp
+    | Tick Time.Time
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -42,21 +101,60 @@ update msg model =
         NoOp ->
             model ! []
 
+        Tick _ ->
+            { model
+                | clkCycle = model.clkCycle + 1
+                , tCycle = changeTCycleState model.tCycle
+                , mCycle = changeMCycleState model.tCycle model.mCycle
+            }
+                ! []
 
 
--- View
+changeTCycleState : TCycle -> TCycle
+changeTCycleState tCycle =
+    case tCycle of
+        T1 ->
+            T2
+
+        T2 ->
+            T3
+
+        T3 ->
+            T4
+
+        T4 ->
+            T1
+
+
+changeMCycleState : TCycle -> MCycle -> MCycle
+changeMCycleState tCycle mCycle =
+    case tCycle of
+        T4 ->
+            case mCycle of
+                M1 ->
+                    M2
+
+                M2 ->
+                    M3
+
+                M3 ->
+                    M1
+
+        _ ->
+            mCycle
 
 
 view : Model -> Html Msg
 view model =
-    h1 []
-        [ text ("Hello " ++ (Utils.capitalize model.name)) ]
-
-
-
--- Subscriptions
+    div []
+        [ h1 []
+            [ text "Z80 Emulator" ]
+        , p [] [ text ("PC: " ++ (toString <| model.clkCycle)) ]
+        , p [] [ text ("MCycle: " ++ (toString <| model.mCycle)) ]
+        , p [] [ text ("TCycle: " ++ (toString <| model.tCycle)) ]
+        ]
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
+    Time.every (1 * Time.second) Tick
